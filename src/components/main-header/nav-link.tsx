12514c8961ba49fe46e9e89f1fdd6aadd5888f41
Eder Andrew <ederandrew0028@gmail.com>
2024-04-22 15:37:52 -0300

    Create home page meals

---
+++ src/components/main-header/nav-link.tsx
@@ -0,0 +1,18 @@
+'use client'
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import classes from './nav-link.module.css'
+
+type Props = {
+    href: string,
+    children: React.ReactNode
+}
+const NavLink = ({href, children}: Props) => {
+    const path = usePathname()
+
+    return (
+        <Link href={href} className={path.startsWith(href) ? `${classes.link} ${classes.active}` : classes.link}>{children}</Link>
+    )
+}
+
+export default NavLink
